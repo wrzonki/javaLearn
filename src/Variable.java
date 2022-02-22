514edbd2b26abe55cfcbf46716ebb1875b21a6db
Pawel Wrzosek <wrzonki@gmail.com>
2022-02-22 21:36:26 +0100

    Lesson 2

---
+++ src/Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+  public static void main(String[] args) {
+    int passengers = 5;
+    System.out.println("There are " + passengers + " passengers.");
+
+    int bysTickets = passengers;
+    System.out.println("There are " + bysTickets + " bys tickets.");
+  }
+}
